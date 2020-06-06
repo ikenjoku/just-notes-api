631f17541c5ec763ccd85ef51ba9bf5c9107a6bc
Ike Njoku <ikeenjoku@gmail.com>
2020-06-06 13:07:39 +0100

    create graphQL service

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 const express = require('express')
+const {
+  ApolloServer,
+  gql
+} = require('apollo-server-express')
+const notes = require('./data/notes')
 
 const app = express()
 const port = process.env.PORT || 1414
 
-app.get('/', (req, res) => {
-  res.send('Good to go')
+const typeDefs = gql `
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
+  type Query {
+    hello: String
+    notes: [Note]!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => 'still Good to go',
+    notes: () => notes
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
 })
 
-app.listen(port, () => {
-  console.log(`Running: listening on port ${port}`)
+server.applyMiddleware({
+  app,
+  path: '/api'
+})
+
+app.listen({
+  port
+}, () => {
+  console.log(`GraphQL Server Running: http://localhost:${port}${server.graphqlPath}`)
 })
