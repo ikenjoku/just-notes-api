2c098e5d96aa373d5bfa1e6e9e786cd147cd22a1
Ike Njoku <ikeenjoku@gmail.com>
2020-06-06 19:40:52 +0100

    add new resolvers

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,10 +1,50 @@
 module.exports = {
-  newNote: async (parent, args, {
+  newNote: async (parent, {
+    content
+  }, {
     models
   }, info) => {
-    return await models.Note.create({
-      content: args.content,
-      author: 'Ike Njoku'
-    })
-  }
+    try {
+      return await models.Note.create({
+        content,
+        author: 'Ike Njoku'
+      })
+    } catch (error) {
+      console.log('Error creating note')
+    }
+  },
+  updateNote: async (parent, {
+    content,
+    id
+  }, {
+    models
+  }, info) => {
+    try {
+      return await models.Note.findOneAndUpdate({
+        _id: id
+      }, {
+        $set: {
+          content
+        }
+      }, {
+        new: true
+      })
+    } catch (error) {
+      console.log('Error updating note')
+    }
+  },
+  deleteNote: async (parent, {
+    id
+  }, {
+    models
+  }, info) => {
+    try {
+      await models.Note.findOneAndDelete({
+        _id: id
+      })
+      return true
+    } catch (error) {
+      return false
+    }
+  },
 }
