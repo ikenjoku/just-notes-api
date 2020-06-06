ecd62f6a97f55e4a3a1b2e15e99893a6166de02d
Ike Njoku <ikeenjoku@gmail.com>
2020-06-06 18:36:59 +0100

    add models to context

--- src/index.js
+++ src/index.js
@@ -2,54 +2,27 @@ require('dotenv').config()
 const db = require('./db')
 const express = require('express')
 const {
-  ApolloServer,
-  gql
+  ApolloServer
 } = require('apollo-server-express')
+const resolvers = require('./resolvers')
+const typeDefs = require('./schema')
 const models = require('./data/models')
 
 const app = express()
 const port = process.env.PORT || 1414
 const DB_URL = process.env.DB_URL
 
-const typeDefs = gql `
-  type Note {
-    id: ID!
-    content: String!
-    author: String!
-  }
-
-  type Query {
-    hello: String
-    notes: [Note]!
-    note(id:ID!): Note!
-  }
-
-  type Mutation {
-    newNote(content: String!): Note!
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello: () => 'still Good to go',
-    notes: async () => await models.Note.find({}),
-    note: async (parent, args, context, info) => await models.Note.findById(args.id)
-  },
-  Mutation: {
-    newNote: async (parent, args, context, info) => {
-      return await models.Note.create({
-        content: args.content,
-        author: 'Ike Njoku'
-      })
-    }
-  }
-}
-
 db.connect(DB_URL)
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: () => {
+
+    return {
+      models
+    }
+  }
 })
 
 server.applyMiddleware({
