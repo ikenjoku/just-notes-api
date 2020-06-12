ed5c6ca2b4257cafc24ed0f9f440676f97a3fbae
Ike Njoku <ikeenjoku@gmail.com>
2020-06-12 14:55:49 +0100

    limit query depth

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
-const db = require('./db')
 const express = require('express')
-const {
-  ApolloServer
-} = require('apollo-server-express')
-const resolvers = require('./resolvers')
+const helmet = require('helmet')
+const cors = require('cors')
+const { ApolloServer } = require('apollo-server-express')
+const deepthLimit = require('graphql-depth-limit')
+const { createComplexityLimitRule } = require('graphql-validation-complexity')
+
+const db = require('./db')
 const typeDefs = require('./schema')
 const models = require('./data/models')
+const resolvers = require('./resolvers')
 const {
   validateJWT
 } = require('./util/authHelpers')
@@ -15,14 +18,18 @@ const app = express()
 const port = process.env.PORT || 1414
 const DB_URL = process.env.DB_URL
 
+app.use(helmet())
+app.use(cors())
 db.connect(DB_URL)
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: async ({
-    req
-  }) => {
+  validationRules: [
+    deepthLimit(5),
+    createComplexityLimitRule(1000)
+  ],
+  context: async ({ req }) => {
     const token = req.headers.authorization
     const user = await validateJWT(token)
 
@@ -33,13 +40,7 @@ const server = new ApolloServer({
   }
 })
 
-server.applyMiddleware({
-  app,
-  path: '/api'
-})
-
-app.listen({
-  port
-}, () => {
+server.applyMiddleware({ app, path: '/api' })
+app.listen({ port }, () => {
   console.log(`GraphQL Server Running: http://localhost:${port}${server.graphqlPath}`)
 })
