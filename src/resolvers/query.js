826a5c860e23b2ae37139557522b6c3add1a8926
Ike Njoku <ikeenjoku@gmail.com>
2020-06-12 14:54:45 +0100

    paginate resource queries

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,7 +1,7 @@
 const { AuthenticationError, ValidationError, ForbiddenError } = require('apollo-server-express')
 
 module.exports = {
-  notes: async (parent, args, { models }, info) => await models.Note.find({}),
+  notes: async (parent, args, { models }, info) => await models.Note.find().limit(50),
 
   note: async (parent, { id }, { models }, info) =>{
     try {
@@ -20,10 +20,34 @@ module.exports = {
   },
 
   users: async (parent, args, { models }, info) => {
-    return await models.User.find({})
+    return await models.User.find().limit(100)
   },
 
   me: async (parent, args, { models, user }, info) => {
     return await models.User.findById(user.id)
+  },
+
+  noteFeed: async (parent, { cursor, limit = 10 }, { models }, info) => {
+    let hasNextPage = false
+    let cursorQuery = {}
+    if (cursor) {
+      cursorQuery = { _id: { $lt: cursor } }
+    }
+      let notes = await models.Note.find(cursorQuery)
+      .sort({_id: -1})
+      .limit(limit + 1)
+
+      if(notes.length > limit) {
+        hasNextPage = true
+        notes = notes.slice(0, -1)
+      }
+
+      const newCursor = notes[notes.length - 1]._id
+
+      return {
+        notes,
+        cursor: newCursor,
+        hasNextPage
+      }
   }
 }
