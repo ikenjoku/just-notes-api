eea7339920a4b6f971ba3f8336961fa84a38ec9c
Ike Njoku <ikeenjoku@gmail.com>
2020-06-12 12:42:03 +0100

    add user query resolvers

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -3,9 +3,13 @@ const {
 } = require('graphql-iso-date')
 const Query = require('./query')
 const Mutation = require('./mutation')
+const User = require('./user.js')
+const Note = require('./note.js')
 
 module.exports = {
   Query,
   Mutation,
+  Note,
+  User,
   DateTime: GraphQLDateTime
 }
